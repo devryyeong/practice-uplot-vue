26980397a65fe872a121fa20e2a4ffe2af2e48db
nrlee <nrlee@pabloair.com>
2025-07-02 17:09:01 +0900

    feat: tailwindcss, shadcn-vue 설치 및 설정

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex">
+    <SyncChart />
+    <Toggle />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
+<script setup lang="ts">
+import SyncChart from "@/components/SyncChart.vue";
+import Toggle from "@/components/Toggle.vue";
+</script>
 <style scoped></style>
